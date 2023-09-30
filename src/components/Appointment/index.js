10464dcc87611518417f5676c5078aaa7a0a4664
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-09-29 21:45:52 -0400

    setup transition between empty, show, form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,25 +1,50 @@
 import React from "react";
 
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
-import Header from "components/Appointment/Header";
-
-import Empty from "components/Appointment/Empty";
-import Show from "components/Appointment/Show";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={() => console.log("CONFIRM")}
-          onEdit={() => console.log("EDIT")}
+          onEdit={() => transition(EDIT)}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onSave={() => console.log("onSave")}
+          onCancel={back}
+        ></Form>
+      )}
+      {mode === EDIT && (
+        <Form
+          name={props.interview.student}
+          interviewers={props.interviewers}
+          interviewer={props.interview.interviewer.id}
+          onSave={() => console.log("onSave")}
+          onCancel={back}
+        ></Form>
+      )}
     </article>
   );
 }
