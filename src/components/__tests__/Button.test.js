eee97069b0e5caa14b471e223b2d01b3bf04d68e
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-01 18:14:12 -0400

    Setup basic test for button rendering

---
+++ src/components/__tests__/Button.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Button from "components/Button";
+
+describe("Button", () => {
+  it("renders without crashing", () => {
+    render(<Button />);
+  });
+  it("renders its `children` prop as text", () => {
+    const { getByText } = render(<Button>Default</Button>);
+    expect(getByText("Default")).toBeInTheDocument();
+  });
+});
